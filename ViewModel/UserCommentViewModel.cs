8be0471da870d43de24b4b8c2cfb386c98ba1e28
Mihrinur İlunt <93780055+mihrinurilunt@users.noreply.github.com>
2023-06-01 16:36:21 +0300

    Add files via upload

---
+++ ViewModel/UserCommentViewModel.cs
@@ -0,0 +1,9 @@
+﻿using CupcakeMVC.Models;
+namespace CupcakeMVC.ViewModel
+{
+    public class UserCommentViewModel
+    {
+        public  UserInfo ?userinfo { get; set; }
+        public List< Comment>?comments {get; set; }
+    }
+}
