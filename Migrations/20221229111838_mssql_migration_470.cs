349c95f37edd9d0306d27ff8f036d807616ef8fe
Mihrinur İlunt <93780055+mihrinurilunt@users.noreply.github.com>
2023-06-01 16:35:01 +0300

    Add files via upload

---
+++ Migrations/20221229111838_mssql_migration_470.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CupcakeMVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class mssqlmigration470 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Desserts",
+                columns: table => new
+                {
+                    DessertId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DessertPhotoURL = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Price = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Desserts", x => x.DessertId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Drink",
+                columns: table => new
+                {
+                    DrinksId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DrinkPhotoURL = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Price = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Drink", x => x.DrinksId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DoubleMenus",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Price = table.Column<int>(type: "int", nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    StartDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DoubleMenuCategory = table.Column<int>(type: "int", nullable: false),
+                    DrinksId = table.Column<int>(type: "int", nullable: false),
+                    DessertId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DoubleMenus", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DoubleMenus_Desserts_DessertId",
+                        column: x => x.DessertId,
+                        principalTable: "Desserts",
+                        principalColumn: "DessertId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DoubleMenus_Drink_DrinksId",
+                        column: x => x.DrinksId,
+                        principalTable: "Drink",
+                        principalColumn: "DrinksId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DoubleMenus_DessertId",
+                table: "DoubleMenus",
+                column: "DessertId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DoubleMenus_DrinksId",
+                table: "DoubleMenus",
+                column: "DrinksId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DoubleMenus");
+
+            migrationBuilder.DropTable(
+                name: "Desserts");
+
+            migrationBuilder.DropTable(
+                name: "Drink");
+        }
+    }
+}
